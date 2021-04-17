268428a6d6f4936bbe7820d2989bcf581f0d82ef
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-04-17 23:16:08 +0530

    Add files via upload

---
+++ printting pattern 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main ()
+{int i,j ;
+printf("programe to display printtting pattern \n");
+for (i=1;i<=5;i++)
+{ for(j=1;j<=5;j++)
+{
+printf("* \t"); }
+
+printf("\n");}
+}
+
