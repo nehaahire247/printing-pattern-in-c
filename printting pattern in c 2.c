3bd66ffba03623378fdaa9a633f27806f819ed26
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-04-17 23:19:42 +0530

    Add files via upload

---
+++ printting pattern in c 2.c
@@ -0,0 +1,21 @@
+/* program to print *
+                    * *
+                    * * *
+                    * * * *
+                    * * * * *
+
+                                         */
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main ()
+{int i,j ;
+printf("programe to display printtting pattern \n");
+for (i=1;i<=5;i++)
+{ for(j=1;j<=i;j++)
+{
+printf("i"); }
+
+printf("\n");}
+}
+
