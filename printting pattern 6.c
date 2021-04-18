300e2e24674a4c511d60ecc8bcfdd726f3aa161d
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-04-18 23:42:25 +0530

    Add files via upload

---
+++ printting pattern 6.c
@@ -0,0 +1,24 @@
+/*  program to print
+1 1 1 1 1 1
+2 2 2 2 2 2
+3 3 3 3 3 3
+4 4 4 4 4 4
+5 5 5 5 5 5         */
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main ()
+{int i,j;
+printf("programe to display printtting pattern \n");
+for (i=1;i<=5;i++)
+{ printf("\n");
+    for(j=1;j<=5;j++)
+    {
+        printf("\t %d ",i);
+
+    }
+}
+
+ printf("\n");
+}
