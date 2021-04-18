55646eec6cbc2d189e40bc7cc268cf62b7cdc4e4
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-04-18 23:44:27 +0530

    Add files via upload

---
+++ printting pattern 8.c
@@ -0,0 +1,25 @@
+/*  program to print
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+  */
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main ()
+{int i,j,K=1;
+printf("programe to display printtting pattern \n");
+for (i=1;i<=5;i++)
+{ printf("\n");
+    for(j=1;j<=5;j++)
+    {
+        printf("\t %d ",j);
+        K++;
+    }
+}
+
+ printf("\n");
+}
